b6e7a34eb458dbdc3b605cd6f4df696ed51e7f1d
jeremie-passerat <jeremie.passerat@etu.univ-amu.fr>
2017-10-04 15:44:52 +0200

    Début exercice 7

---
+++ src/main/java/fr/univ_amu/iut/DAO/JDBC/DAOModuleJDBC.java
@@ -0,0 +1,14 @@
+
+import fr.univ_amu.iut.DAO.DAOModule;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class DAOModuleJDBC implements DAOModule {
+    @Override
---
+++ src/main/java/fr/univ_amu/iut/DAO/JDBC/DAOProfJDBC.java
@@ -0,0 +1,14 @@
+
+import fr.univ_amu.iut.DAO.DAOProf;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class DAOProfJDBC implements DAOProf {
+    @Override
